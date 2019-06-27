76f1fe8e9cb57cc5ce3370aabdd3e4da8e54ec60
voson wang <voson@ricnsmart.com>
2019-06-27 10:04:40 +0800

    feat(rcn-vj): 增加rcnvj结构体

---
+++ rcn-vj.go
@@ -0,0 +1,33 @@
+package plugins
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"time"
+)
+
+type RCNVJ struct {
+	ID           primitive.ObjectID `bson:"_id,omitempty"`
+	CreateAt     string             `bson:"CreateAt"`
+	SN           string             `bson:"SN"`
+	UpdateAt     string             `bson:"UpdateAt"`
+	DeviceID     string             `bson:"DeviceID"`
+	GPRSOperator int                `bson:"GPRSOperator"`
+	DomainRecord string             `bson:"DomainRecord"`
+	Interval     int                `bson:"Interval"`
+	Version      float32            `bson:"Version"`
+	GPRSRSSI     int                `bson:"GPRSRSSI"`
+	ModulesNum   int                `bson:"ModulesNum"`
+}
+
+func (i *RCNVJ) Find(id string, coll *mongo.Collection) (err error) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	filter := bson.M{ID: id}
+
+	err = coll.FindOne(ctx, filter).Decode(i)
+
+	return
+}
