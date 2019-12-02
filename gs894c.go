22d6211ac9995167f8dfd3b540f96f68a0d98d78
voson wang <voson@ricnsmart.com>
2019-12-02 18:52:47 +0800

    refactor(plugin): GS894C

--- gs894c.go
+++ gs894c.go
@@ -12,4 +12,10 @@ type GS894C struct {
 	DeviceID   string             `bson:"DeviceID"`
 	DeviceType int                `bson:"DeviceType"`
 	OneNetID   string             `bson:"OneNetID"`
+	Metrics    struct {
+		CH4 struct {
+			AlertSwitch bool `bson:"AlertSwitch"`
+			SMSSwitch   bool `bson:"SMSSwitch"`
+		} `bson:"CH4"`
+	} `bson:"Metrics"`
 }
